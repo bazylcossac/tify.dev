545f1b078fa7c7859b3b12be4eb2ac872d10602e
bazylcossac <bazyl.cossac@gmail.com>
2025-03-08 14:33:45 +0100

    another links fix, i shouldn't give this task to chatgpt

--- src/lib/formatText.tsx
+++ src/lib/formatText.tsx
@@ -4,7 +4,9 @@ import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 export default function formatText(text: string) {
   return text
-    ?.split(/(```[\s\S]*?```|#\S+|https?:\/\/www\.youtube\.com\/watch\S+)/g)
+    ?.split(
+      /(```[\s\S]*?```|#\S+|https?:\/\/(?:www\.youtube\.com\/watch\S+|youtu\.be\/\S+))/g
+    )
     ?.map((part, index) => {
       if (part.startsWith("#")) {
         return (
