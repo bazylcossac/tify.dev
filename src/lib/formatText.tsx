c72926b6f8a8ae10b790cbf321512e9b21f67dfe
bazylcossac <bazyl.cossac@gmail.com>
2025-03-08 14:27:45 +0100

    fixed bug with double iframe

--- src/lib/formatText.tsx
+++ src/lib/formatText.tsx
@@ -17,18 +17,8 @@ export default function formatText(text: string) {
         part.startsWith("https://www.youtube.com/watch") ||
         part.startsWith("https://youtu.be/")
       ) {
-        const videoId = part.includes("watch?v=")
-          ? part.split("watch?v=")[1].split("&")[0]
-          : part.split("youtu.be/")[1].split("?")[0];
-
         return (
           <div key={index} className="my-4">
-            <iframe
-              src={`https://www.youtube.com/embed/${videoId}`}
-              className="w-full h-[500px] rounded-lg"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
             <Link href={part} target="_blank">
               <span className="text-blue-500 font-bold block mt-2">{part}</span>
             </Link>
